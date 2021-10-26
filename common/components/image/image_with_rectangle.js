2fae1c361b1e937e97adf25e1bf1055d97a46738
thomas rodier <rodiert17@gmail.com>
2021-10-26 07:12:30 +0200

    try to fix issue with video not playing in mobile #3

--- common/components/image/image_with_rectangle.js
+++ common/components/image/image_with_rectangle.js
@@ -4,7 +4,7 @@ import imageStyle from '../../styles/components/image.module.css'
 export default function Image_With_Rectangle({src, alt, width, height, type}) {
     const renderVideoOrImage = () => {
         if(type === "video") {
-            return <video controls playsInline autoPlay muted loop src="https://res.cloudinary.com/dad2k5sll/video/upload/v1630399800/demo_product_homepage_i5sckd.webm"></video>              
+            return <video controls playsinline autoPlay muted loop src="https://res.cloudinary.com/dad2k5sll/video/upload/v1630399800/demo_product_homepage_i5sckd.webm"></video>              
         }
         else if(type === "image"){
             return <Image
