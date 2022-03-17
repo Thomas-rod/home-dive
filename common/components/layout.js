2cf67116de0337ed546bdcad996fe95c2eda8b3c
Thomas Rodier <rodiert17@gmail.com>
2022-03-17 20:03:01 +0100

    add segments in header

--- common/components/layout.js
+++ common/components/layout.js
@@ -54,7 +54,14 @@ export default function Layout({ children}) {
                 })(window,document,'script','dataLayer','GTM-PNBLMX4');`
             }}
           />
-
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="UFd3p9pk2UuA7TdDMtQ0d5SzBGKDZKoI";;analytics.SNIPPET_VERSION="4.15.3";
+                analytics.load("UFd3p9pk2UuA7TdDMtQ0d5SzBGKDZKoI");
+                analytics.page();
+                }}();`
+            }}
+          />
           {/* <script data-ad-client="ca-pub-5619393240417230" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
         </Head>
 
